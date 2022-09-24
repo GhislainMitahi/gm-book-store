f5942ffabe8526b57686c5286f66aa48e54392b7
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-09-24 10:25:08 +0100

    linters fixing

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Category, Books } from './components/component';
 
 const App = () => (
-  <div className=''>
+  <div>
     <Router>
       <Routes>
-        <Route path='/books' index element={<Books />} />
+        <Route path='/' index element={<Books />} />
         <Route path='/category' element={<Category />} />
       </Routes>
     </Router>
