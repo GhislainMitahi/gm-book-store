385633b6e0e80c344fb406670a8a4678476238f1
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-09-24 06:30:44 +0100

    add book

--- src/components/books/book.js
+++ src/components/books/book.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const Book = () => (
+const Book = () => {
+
+
+  return(
   <main>
     <div className="card">
       <h2>First Book</h2>
@@ -15,5 +18,5 @@ const Book = () => (
       <button type="button">Remove</button>
     </div>
   </main>
-);
+)};
 export default Book;
